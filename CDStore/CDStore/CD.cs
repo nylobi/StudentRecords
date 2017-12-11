6ab52d23fa674f765d60809ef158dff3f2161539
Nyle Neckel <nylobi@gmail.com>
2017-12-11 09:36:53 +0000

    ORM start

---
+++ CDStore/CDStore/CD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDStore
+{
+    class CD
+    {
+        public int CDid { get; set; }
+
+        public string Title { get; set; }
+
+        public string RecordCompany { get; set; }
+
+        public DateTime Published { get; set; }
+
+        public string[] song { get; set; }
+    }
+}
